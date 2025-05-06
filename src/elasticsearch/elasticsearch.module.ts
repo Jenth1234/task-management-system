6f36d9c3cb10c2110fe57482ddc105993e744be3
thong <thongviet@gmail.com>
2025-05-06 15:13:54 +0700

    update

---
+++ src/elasticsearch/elasticsearch.module.ts
@@ -0,0 +1,12 @@
+// import { Module } from '@nestjs/common';
+// import { ElasticsearchService } from './elasticsearch.service';
+
+// @Module({
+//   imports: [
+//     ElasticsearchModule.register({
+//       node: 'http://localhost:9200',
+//     }),
+//   ],
+//   providers: [ElasticsearchService],
+// })
+// export class ElasticsearchModule {}
